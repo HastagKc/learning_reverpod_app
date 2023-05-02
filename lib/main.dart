c29683f84eb2d87757f53ce4784223ec8db5148e
HastagKc <chaudharykshittiz950@gmail.com>
2023-05-02 17:35:00 +0545

    Provider class is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:get/get.dart';
 import 'package:riverpod_learning/model/home_page.dart';
 
